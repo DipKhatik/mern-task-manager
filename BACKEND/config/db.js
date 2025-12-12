3452d5dae21ee9cad9a8524d377afd46434ba52f
deep khatik <deepkhatik31122003@gmail.com>
2025-12-12 11:02:18 +0530

    Initial MERN Todo App commit

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -1,12 +1,23 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () =>{
+//     try{
+//         const conn = await mongoose.connect(process.env.MONGO_URI);
+//         console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     }
+//     catch(error){
+//         console.log(`Error: ${error.message}`);
+//         process.exit(1);
+//     }
+// }
 import mongoose from "mongoose";
 
-export const connectDB = async () =>{
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    }
-    catch(error){
-        console.log(`Error: ${error.message}`);
-        process.exit(1);
-    }
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB Error:", error.message);
+    process.exit(1);
+  }
+};
